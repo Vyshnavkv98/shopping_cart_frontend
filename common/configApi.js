925b2c9923ae3fdf9cde653cbb323a44fb6d7e17
vyshnavkv <vyshnavkv89@gmail.com>
2024-06-26 18:41:11 +0530

    endpoints changed

--- common/configApi.js
+++ common/configApi.js
@@ -5,83 +5,83 @@ const backendDomain = "https://shopping-cart-backend-2bp3.onrender.com/api";
 const endPoints = {
 
     singUp: {
-        url: `${backendDomain}/auth/signup`,
+        url: `${backendDomain}auth/signup`,
         method: "POST"
     },
     logIn: {
-        url: `${backendDomain}/auth/login`,
+        url: `${backendDomain}auth/login`,
         method: "POST"
     },
     current_user: {
-        url: `${backendDomain}/user-details`,
+        url: `${backendDomain}user-details`,
         method: "GET"
     },
     user_logout: {
-        url: `${backendDomain}/user-logout`,
+        url: `${backendDomain}user-logout`,
         method: "GET"
     },
     all_users: {
-        url: `${backendDomain}/all-users`,
+        url: `${backendDomain}all-users`,
         method: "GET"
     },
     update_User_role: {
-        url: `${backendDomain}/update-user-role`,
+        url: `${backendDomain}update-user-role`,
         method: "POST"
     },
     uploadProduct: {
-        url: `${backendDomain}/upload-product`,
+        url: `${backendDomain}upload-product`,
         method: "POST"
     },
     allProducts: {
-        url: `${backendDomain}/get-all-products`,
+        url: `${backendDomain}get-all-products`,
         method: "Get"
     },
     updateProduct: {
-        url: `${backendDomain}/update-product`,
+        url: `${backendDomain}update-product`,
         method: "POST"
     },
     getProductCategory: {
-        url: `${backendDomain}/get-category-product`,
+        url: `${backendDomain}get-category-product`,
         method: "GET"
     },
     getCategoryWiseProducts: {
-        url: `${backendDomain}/get-categoryWise-product`,
+        url: `${backendDomain}get-categoryWise-product`,
         method: "POST"
     },
     getProductDetails: {
-        url: `${backendDomain}/get-product-details`,
+        url: `${backendDomain}get-product-details`,
         method: "POST",
     },
     addToCartProduct: {
-        url: `${backendDomain}/addToCart`,
+        url: `${backendDomain}addToCart`,
         method: "POST"
     },
     countAddToCartProduct: {
-        url: `${backendDomain}/count-addtocart-product`,
+        url: `${backendDomain}count-addtocart-product`,
         method: "GET"
     },
     cartViewProduct: {
-        url: `${backendDomain}/view-cart-product`,
+        url: `${backendDomain}view-cart-product`,
         method: "GET"
     },
     updateCartProduct: {
-        url: `${backendDomain}/update-cart-product`,
+        url: `${backendDomain}update-cart-product`,
         method: "POST"
     },
     deleteCartProduct: {
-        url: `${backendDomain}/delete-cart-product`,
+        url: `${backendDomain}delete-cart-product`,
         method: "POST"
     },
     searchProducts: {
-        url: `${backendDomain}/search-products`,
+        url: `${backendDomain}search-products`,
         method: "POST"
     },
     filterProducts: {
-        url: `${backendDomain}/filter-products`,
+        url: `${backendDomain}filter-products`,
         method: "POST"
     },
     payment: {
-        url: `${backendDomain}/checkout`,
+        url: `${backendDomain}checkout`,
         method: "POST"
     },
 };
